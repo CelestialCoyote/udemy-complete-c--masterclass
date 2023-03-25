f4f2399f54cb632881e225004d0acf5c9557b8f6
Paul Stearns <celestialcoyote@gmail.com>
2023-03-25 14:12:48 -0700

    added StringManipulation

---
+++ Section02/StringExercises/StringExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringExercises
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
