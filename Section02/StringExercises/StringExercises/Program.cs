ac3fa09e4e4fd24cdb59ab34e94d434d06e3bc1e
Paul Stearns <celestialcoyote@gmail.com>
2023-03-25 16:03:49 -0700

    StringExercies

--- Section02/StringExercises/StringExercises/Program.cs
+++ Section02/StringExercises/StringExercises/Program.cs
@@ -6,7 +6,18 @@ namespace StringExercises
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name;
+
+
+            Console.WriteLine("Please enter your name.");
+            name = Console.ReadLine();
+
+            Console.WriteLine($"Name to UpperCase - {name.ToUpper()}");
+            Console.WriteLine($"Name to LowerCase - {name.ToLower()}");
+            Console.WriteLine($"Remove leading and trailing white-space - {name.Trim()}");
+            Console.WriteLine($"Get 2 characters starting at index 2 - {name.Substring(2, 2)}");
+
+            Console.ReadKey();
         }
     }
 }
