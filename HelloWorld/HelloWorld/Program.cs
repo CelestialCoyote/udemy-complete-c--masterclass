bf689fb9250c4117f1c4418f65535d9822f37a46
Paul Stearns <celestialcoyote@gmail.com>
2023-03-25 13:15:35 -0700

    initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section02/ParsingExercise/ParsingExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ParsingExercise
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string stringForFloat = "0.85"; // datatype should be float
+            string stringForInt = "12345"; // datatype should be int
+
+            float myFloat = float.Parse(stringForFloat);
+            int MyInt = Int32.Parse(stringForInt);
+
+            Console.WriteLine($"myFloat = {myFloat}, myInt = {MyInt}");
+            Console.ReadKey();
+        }
+    }
+}
