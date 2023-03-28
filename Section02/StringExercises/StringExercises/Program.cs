284d038b2d6d454ce0aedf46e487cc4a232d87be
Paul Stearns <celestialcoyote@gmail.com>
2023-03-28 14:43:34 -0700

    String challenges

--- Section02/StringExercises/StringExercises/Program.cs
+++ Section02/StringExercises/StringExercises/Program.cs
@@ -6,16 +6,36 @@ namespace StringExercises
     {
         public static void Main(string[] args)
         {
-            string name;
+            // Challenge 1.
+            //string name;
 
 
-            Console.WriteLine("Please enter your name.");
-            name = Console.ReadLine();
+            //Console.WriteLine("Please enter your name.");
+            //name = Console.ReadLine();
 
-            Console.WriteLine($"Name to UpperCase - {name.ToUpper()}");
-            Console.WriteLine($"Name to LowerCase - {name.ToLower()}");
-            Console.WriteLine($"Remove leading and trailing white-space - {name.Trim()}");
-            Console.WriteLine($"Get 2 characters starting at index 2 - {name.Substring(2, 2)}");
+            //Console.WriteLine($"Name to UpperCase - {name.ToUpper()}");
+            //Console.WriteLine($"Name to LowerCase - {name.ToLower()}");
+            //Console.WriteLine($"Remove leading and trailing white-space - {name.Trim()}");
+            //Console.WriteLine($"Get 2 characters starting at index 2 - {name.Substring(2, 2)}");
+
+            //Console.ReadKey();
+
+
+            // Challenge 2.
+            Console.WriteLine("Please enter a string.");
+            string str = Console.ReadLine();
+            Console.WriteLine("Please enter a character to be searched.");
+            char character = Console.ReadLine()[0];
+
+            Console.WriteLine($"{character} appears at index {str.IndexOf(character)}");
+
+
+            Console.WriteLine("Please enter first name.");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Please enter last name.");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Full Name - {firstName} {lastName}");
 
             Console.ReadKey();
         }
