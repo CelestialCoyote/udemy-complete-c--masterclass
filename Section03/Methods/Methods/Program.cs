55d9ecb248532145f49b2498982c26f19a68c727
Paul Stearns <celestialcoyote@gmail.com>
2023-03-28 17:05:22 -0700

    Finished section 3

---
+++ Section03/Methods/Methods/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Methods
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string friend1 = "Kalani";
+            string friend2 = "Tikaa";
+            string friend3 = "Jackson";
+
+
+            //WriteSomething();
+            //WriteSomethingSpecific("specific text");
+            //Console.WriteLine(Add(15, 31));
+            //Console.WriteLine(Multiply(25, 25));
+            //Console.WriteLine(Divide(25, 13));
+            //Console.WriteLine(GreetFriend(friend1));
+            //Console.WriteLine(GreetFriend(friend2));
+            //Console.WriteLine(GreetFriend(friend3 ));
+            Console.WriteLine(LowUpper("HeY ThErE !"));
+            Count("HeY ThErE !");
+
+            Console.Read();
+        }
+
+
+        public static void WriteSomething()
+        {
+            Console.WriteLine("Called from main method.");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public static string GreetFriend(string friend)
+        {
+            return $"Hi {friend}, my friend!";
+        }
+
+        public static string LowUpper(string text)
+        {
+            string lower = text.ToLower();
+            string upper = text.ToUpper();
+
+            return $"{lower}{upper}";
+        }
+
+        public static void Count(string text)
+        {
+            Console.WriteLine($"The amount of characters is {text.Length}.");
+        }
+    }
+}
---
+++ Section03/TryCatch/TryCatch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TryCatch
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Format exception, try again.");
+            }
+            finally
+            {
+                Console.WriteLine("This is called always.");
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
