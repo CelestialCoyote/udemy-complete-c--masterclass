fd76aba47dc61531dde5c6b58455839aec970b54
Paul Stearns <celestialcoyote@gmail.com>
2023-04-02 15:41:50 -0700

    Login exercise with if statements

---
+++ Section04/LogIn/LogIn/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LogIn
+{
+    class MainClass
+    {
+        static string userName;
+        static string password;
+
+
+        public static void Main(string[] args)
+        {
+            Register();
+            Login();
+            Console.Read();
+        }
+
+        public static void Register()
+        {
+            Console.WriteLine("Please enter your username.");
+            userName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your password");
+            password = Console.ReadLine();
+
+            Console.WriteLine("Registration completed.");
+            Console.WriteLine("------------------------------");
+        }
+
+        public static void Login()
+        {
+            Console.WriteLine("Please enter your username");
+
+            if (userName == Console.ReadLine())
+            {
+                Console.WriteLine("Please enter your password.");
+
+                if (password == Console.ReadLine())
+                {
+                    Console.WriteLine("Login successfull.");
+                }
+                else
+                {
+                    Console.WriteLine("Login failed, wrong password. Restart Program.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Login failed, wrong username. Restart Program.");
+            }
+        }
+    }
+}
