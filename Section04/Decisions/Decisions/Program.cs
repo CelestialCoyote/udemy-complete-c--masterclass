a7030cc31082d8081bc9a1eddcd7bf9d3cfc6626
Paul Stearns <celestialcoyote@gmail.com>
2023-04-01 17:32:19 -0700

    Started section 4

---
+++ Section04/Decisions/Decisions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Decisions
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("What's the temperature like?");
+            string temperature = Console.ReadLine();
+            int numTemp;
+            int number;
+
+            if (int.TryParse(temperature, out number))
+                numTemp = number;
+            else
+                numTemp = 0;
+
+            if (numTemp < 20) {
+                Console.WriteLine("Take the coat.");
+            } else if (numTemp == 20) {
+                Console.WriteLine("Casual.");
+            } else {
+                Console.WriteLine("Shorts and t-shirt.");
+            }
+
+            Console.Read();
+        }
+    }
+}
