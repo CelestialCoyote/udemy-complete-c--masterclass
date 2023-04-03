2d3e30f3029ec13d4e385d647af37caf67373111
Paul Stearns <celestialcoyote@gmail.com>
2023-04-03 16:41:35 -0700

    challenges

---
+++ Section04/EnhancedIfStatement/EnhancedIfStatement/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace EnhancedIfStatement
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Temperature in Celcius.
+            int temperature = -5;
+            string stateOfMatter;
+
+            if (temperature < 0)
+                stateOfMatter = "solid";
+            else
+                stateOfMatter = "liquid";
+
+
+            Console.WriteLine($"State of matter is {stateOfMatter}.");
+
+
+            // Use ternary instead of if statement.
+            temperature += 30;
+
+            stateOfMatter = temperature < 0 ? "solid" : "liquid";
+
+            Console.WriteLine($"State of matter is {stateOfMatter}.");
+
+            // Compound ternary.
+            temperature += 100;
+
+            stateOfMatter = temperature > 100 ? "gas" : temperature < 0 ? "solid" : "liquid";
+
+            Console.WriteLine($"State of matter is {stateOfMatter}.");
+
+
+
+            int inputTemperature = 0;
+            string temperatureMessage = string.Empty;
+            string inputValue = string.Empty;
+
+            Console.WriteLine("Enter current temperature: ");
+            inputValue = Console.ReadLine();
+
+            bool validateInput = int.TryParse(inputValue, out inputTemperature);
+
+            if (validateInput)
+            {
+                temperatureMessage = inputTemperature <= 15 ? "too cold" :
+                    (inputTemperature >= 16 && inputTemperature <= 28) ? "it is cold" :
+                    inputTemperature > 28 ? "it is hot" : "";
+                Console.WriteLine(temperatureMessage);
+            }
+            else
+            {
+                Console.WriteLine("Not a valid temperature.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
--- Section04/IfStatementChallenge2/IfStatementChallenge2/Program.cs
+++ Section04/IfStatementChallenge2/IfStatementChallenge2/Program.cs
@@ -4,9 +4,33 @@ namespace IfStatementChallenge2
 {
     class MainClass
     {
+        static int highScore = 300;
+        static string highScorePlayer = "Tikaa";
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            CheckScore(250, "Jackson");
+            CheckScore(315, "Kalaini");
+            CheckScore(330, "Tikaa");
+
+            Console.Read();
+        }
+
+        public static void CheckScore(int score, string player)
+        {
+            if (score > highScore)
+            {
+                highScore = score;
+                highScorePlayer = player;
+
+                Console.WriteLine($"{player} has the new high score is {score}.");
+            }
+            else
+            {
+                Console.WriteLine($"The high score is still {score} held by {player}.");
+               
+            }
         }
     }
 }
