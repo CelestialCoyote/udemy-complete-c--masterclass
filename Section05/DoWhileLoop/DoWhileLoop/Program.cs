5da79a2704f86a17464b86d5185fcd2fcd76b002
Paul Stearns <celestialcoyote@gmail.com>
2023-04-03 18:46:39 -0700

    do loop

---
+++ Section05/DoWhileLoop/DoWhileLoop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DoWhileLoop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int counter = 0;
+            int lengthOfText = 0;
+            string wholeText = "";
+
+            do
+            {
+                Console.WriteLine("Please enter the name of a friend.");
+                string nameOfFriend = Console.ReadLine();
+
+                int currentLength = nameOfFriend.Length;
+
+                lengthOfText += currentLength;
+                wholeText += nameOfFriend;
+            } while (lengthOfText < 20);
+
+            Console.WriteLine($"Thanks, that was enough. {wholeText}");
+
+            Console.Read();
+        }
+    }
+}
---
+++ Section05/ForLoop/ForLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ForLoop
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //for (int counter = 0; counter < 10; counter++)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+
+            for (int counter = 0; counter < 20; counter++)
+            {
+                if (counter % 2 != 0)
+                    Console.WriteLine(counter);
+            }
+
+            Console.Read();
+        }
+    }
+}
