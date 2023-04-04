4ddcde22c0f4660bcfc58c3e150a05454ddb43d4
Paul Stearns <celestialcoyote@gmail.com>
2023-04-04 13:27:56 -0700

    finished loop section

---
+++ Section05/AverageChallenge/AverageChallenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace AverageChallenge
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string input = "0";
+            int count = 0;
+            int totalScore = 0;
+            int currentEntry = 0;
+
+            while (input != "-1")
+            {
+                Console.WriteLine($"Last number was {currentEntry}");
+                Console.WriteLine("Enter next score.");
+                Console.WriteLine($"Total entries {count}");
+                Console.WriteLine($"Enter -1 to calculate average score.");
+
+                input = Console.ReadLine();
+                if (input.Equals("-1"))
+                {
+                    double average = (double) totalScore / (double) count;
+                    Console.WriteLine($"Average score is: {average}");
+                }
+                if (int.TryParse(input, out currentEntry) && currentEntry > 0 && currentEntry <= 20)
+                {
+                    totalScore += currentEntry;
+                }
+                else
+                {
+                    if (!(input.Equals("-1")))
+                    {
+                        Console.WriteLine("Please enter a value between 1 and 20");
+                    }
+                    continue;
+                }
+
+                count++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Section05/BreakContinue/BreakContinue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //for(int counter = 0; counter < 10; counter++)
+            //{
+            //    Console.WriteLine(counter);
+
+            //    if(counter == 3)
+            //    {
+            //        Console.WriteLine("At 3 we stop.");
+            //        break;
+            //    }
+            //}
+
+            Run();
+            Console.Read();
+        }
+
+        public static void Run()
+        {
+            int i = -10;
+
+            while (true)
+            {
+                if (i % 3 == 0)
+                {
+                    i++;
+                    continue;
+                }
+                   
+                if (i == 10)
+                    break;
+
+                // TODO
+                if (i == 11)
+                {
+                    Console.WriteLine(" FINAL BREAK REACHED! This should not happen!");
+                    break;
+                }
+                Console.WriteLine(i++);
+            }
+        }
+    }
+}
