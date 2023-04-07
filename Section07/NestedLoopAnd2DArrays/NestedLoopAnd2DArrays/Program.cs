51a2f8e836d9ee19f5044575fd06fba4d623514c
Paul Stearns <celestialcoyote@gmail.com>
2023-04-06 19:56:52 -0700

    tic-tac-toe challenge

---
+++ Section07/NestedLoopAnd2DArrays/NestedLoopAnd2DArrays/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace NestedLoopAnd2DArrays
+{
+    class MainClass
+    {
+        static int[,] matrix =
+        {
+            { 1, 2, 3 },
+            { 4, 5, 6 },
+            { 7, 8, 9 }
+        };
+
+        public static void Main(string[] args)
+        {
+            foreach (int number in matrix)
+            {
+                Console.WriteLine(number + " ");
+            }
+
+            Console.WriteLine("-------------------");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 == 0)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        Console.Write(matrix[i, j] + " ");
+                    }
+                }
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                Console.WriteLine(matrix[i, i]);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("-------------------");
+
+            for (int i = 0, j = 2; i < matrix.GetLength(0); i++, j--)
+            {
+                Console.WriteLine(matrix[i, j]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section07/TicTacToeChallenge/TicTacToeChallenge/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace TicTacToeChallenge
+{
+    class MainClass
+    {
+        static string[,] game1 =
+        {
+            { "X", "X", "X" },
+            { "O", "5", "6" },
+            { "7", "O", "O" }
+        };
+
+        static string[,] game2 =
+        {
+            { "X", "O", "3" },
+            { "X", "5", "O" },
+            { "X", "O", "9" }
+        };
+
+        static string[,] game3 =
+        {
+            { "X", "O", "3" },
+            { "4", "X", "O" },
+            { "7", "O", "X" }
+        };
+
+        static string[,] game4 =
+        {
+            { "X", "O", "X" },
+            { "X", "O", "O" },
+            { "O", "X", "X" }
+        };
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Checker(game1));
+            Console.WriteLine(Checker(game2));
+            Console.WriteLine(Checker(game3));
+            Console.WriteLine(Checker(game4));
+
+            Console.ReadKey();
+        }
+
+        public static bool Checker(string[,] board)
+        {
+            for (int i = 0; i <= 2; i++)
+            {
+                if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
+                    return true;
+                if (board[0, i] == board[1, i] && board[1, i] == board[2, i])
+                    return true;
+            }
+
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+                return true;
+            if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+                return true;
+
+
+            return false;
+        }
+    }
+}
