41ef4a5f4b33b8decc74b8b259ce5dd4a3a3bda5
Paul Stearns <celestialcoyote@gmail.com>
2023-04-07 17:55:20 -0700

    TicTacToe game

--- Section07/TicTacToeChallenge/TicTacToeChallenge/Program.cs
+++ Section07/TicTacToeChallenge/TicTacToeChallenge/Program.cs
@@ -4,45 +4,138 @@ namespace TicTacToeChallenge
 {
     class MainClass
     {
-        static string[,] game1 =
+        static char[,] playField =
         {
-            { "X", "X", "X" },
-            { "O", "5", "6" },
-            { "7", "O", "O" }
+            {'1', '2', '3' },
+            {'4', '5', '6' },
+            {'7', '8', '9' }
         };
 
-        static string[,] game2 =
-        {
-            { "X", "O", "3" },
-            { "X", "5", "O" },
-            { "X", "O", "9" }
-        };
-
-        static string[,] game3 =
-        {
-            { "X", "O", "3" },
-            { "4", "X", "O" },
-            { "7", "O", "X" }
-        };
-
-        static string[,] game4 =
-        {
-            { "X", "O", "X" },
-            { "X", "O", "O" },
-            { "O", "X", "X" }
-        };
+        static int turns = 0;
 
         public static void Main(string[] args)
         {
-            Console.WriteLine(Checker(game1));
-            Console.WriteLine(Checker(game2));
-            Console.WriteLine(Checker(game3));
-            Console.WriteLine(Checker(game4));
+            int player = 2;
+            int input = 0;
+            bool inputCorrect = true;
 
-            Console.ReadKey();
+            do
+            {
+                if (player == 2)
+                {
+                    player = 1;
+                    EnterXorO('O', input);
+                }
+                else if (player == 1)
+                {
+                    player = 2;
+                    EnterXorO('X', input);
+                }
+
+                SetField();
+
+                #region
+                // Check for winner.
+                if (Checker(playField))
+                {
+                    if (player == 1)
+                        Console.WriteLine("O is the winner!");
+                    else
+                        Console.WriteLine("X is the winner!");
+
+                    Console.WriteLine("Press any key to reset game.");
+                    Console.ReadKey();
+                    Console.WriteLine("");
+                    ResetField();
+                    //break;
+                }
+                else if (turns == 10)
+                {
+                    Console.WriteLine("It is a draw. Press any key to reset game.");
+                    Console.ReadKey();
+                    Console.WriteLine("");
+                    ResetField();
+                }
+                #endregion
+
+                #region
+                // Test is space is already taken.
+                do
+                {
+                    Console.Write($"\nPlayer {player}: Choose your field! ");
+
+                    try
+                    {
+                        input = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter a number between 1 and 9.");
+                    }
+
+                    if (input == 1 && playField[0, 0] == '1')
+                        inputCorrect = true;
+                    else if (input == 2 && playField[0, 1] == '2')
+                        inputCorrect = true;
+                    else if (input == 3 && playField[0, 2] == '3')
+                        inputCorrect = true;
+                    else if (input == 4 && playField[1, 0] == '4')
+                        inputCorrect = true;
+                    else if (input == 5 && playField[1, 1] == '5')
+                        inputCorrect = true;
+                    else if (input == 6 && playField[1, 2] == '6')
+                        inputCorrect = true;
+                    else if (input == 7 && playField[2, 0] == '7')
+                        inputCorrect = true;
+                    else if (input == 8 && playField[2, 1] == '8')
+                        inputCorrect = true;
+                    else if (input == 9 && playField[2, 2] == '9')
+                        inputCorrect = true;
+                    else
+                    {
+                        Console.WriteLine("\nIncorrect input! Please choose another space.");
+                        inputCorrect = false;
+                    }
+
+                } while (!inputCorrect);
+                #endregion
+
+            } while (true);
         }
 
-        public static bool Checker(string[,] board)
+
+        public static void SetField()
+        {
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine($" {playField[0, 0]} | {playField[0, 1]} | {playField[0, 2]} ");
+            Console.WriteLine("___|___|___");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine($" {playField[1, 0]} | {playField[1, 1]} | {playField[1, 2]} ");
+            Console.WriteLine("___|___|___");
+            Console.WriteLine("   |   |   ");
+            Console.WriteLine($" {playField[2, 0]} | {playField[2, 1]} | {playField[2, 2]} ");
+            Console.WriteLine("   |   |   ");
+
+            turns++;
+        }
+
+        public static void EnterXorO(char playerSign, int input)
+        {
+            switch (input)
+            {
+                case 1: playField[0, 0] = playerSign; break;
+                case 2: playField[0, 1] = playerSign; break;
+                case 3: playField[0, 2] = playerSign; break;
+                case 4: playField[1, 0] = playerSign; break;
+                case 5: playField[1, 1] = playerSign; break;
+                case 6: playField[1, 2] = playerSign; break;
+                case 7: playField[2, 0] = playerSign; break;
+                case 8: playField[2, 1] = playerSign; break;
+                case 9: playField[2, 2] = playerSign; break;
+            }
+        }
+
+        public static bool Checker(char[,] board)
         {
             for (int i = 0; i <= 2; i++)
             {
@@ -59,6 +152,20 @@ namespace TicTacToeChallenge
 
 
             return false;
+        }
+
+        public static void ResetField()
+        {
+            char[,] playFieldInitial =
+            {
+                {'1', '2', '3' },
+                {'4', '5', '6' },
+                {'7', '8', '9' }
+            };
+
+            playField = playFieldInitial;
+            SetField();
+            turns = 0;
         }
     }
 }
