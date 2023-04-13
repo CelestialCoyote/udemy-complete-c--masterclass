5c05fcc322e8b162c3aa529fc8ec915c13a0878c
Paul Stearns <celestialcoyote@gmail.com>
2023-04-12 17:27:07 -0700

    arraylist and params

---
+++ Section07/ArrayListExample/ArrayListExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+
+
+namespace ArrayListExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            ArrayList arrayList2 = new ArrayList(100);
+
+
+            arrayList.Add(25);
+            arrayList.Add("Hello");
+            arrayList.Add(13.37);
+            arrayList.Add(13);
+            arrayList.Add(128);
+            arrayList.Add(25.3);
+
+            arrayList.Remove(13);
+
+            arrayList.RemoveAt(0);
+
+            Console.WriteLine(arrayList.Count);
+
+            double sum = 0;
+
+            foreach (object obj in arrayList)
+            {
+                if (obj is int)
+                    sum += Convert.ToDouble(obj);
+                else if (obj is double)
+                    sum += (double)obj;
+                else
+                    Console.WriteLine(obj);
+            }
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section07/ParamsKeyword/ParamsKeyword/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ParamsKeyword
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int min = MinV2(6, 4, 2, 8, 0, 1, 5);
+
+
+            Console.WriteLine($"The minimum value: {min}.");
+            Console.ReadKey();
+        }
+
+
+        public static int MinV2(params int[] numbers)
+        {
+            int min = int.MaxValue;
+
+            foreach (int number in numbers)
+            {
+                if (number < min)
+                    min = number;
+            }
+
+            return min;
+        }
+    }
+}
