c0d580a6ab53f30c0cc68263300eb9c3b9237b7c
Paul Stearns <celestialcoyote@gmail.com>
2023-04-18 19:09:47 -0700

    abstract classes

---
+++ Section10/AbstractClasses/AbstractClasses/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AbstractClasses
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Shape[] shapes = { new Sphere(4), new Cube(3) };
+
+            foreach(Shape shape in shapes)
+            {
+                shape.GetInfo();
+                Console.WriteLine($"{shape.Name} has a volume of {shape.Volume()}.");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Section10/AbstractClasses/AbstractClasses/Sphere.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace AbstractClasses
+{
+    public class Sphere : Shape
+    {
+        public double Radius { get; set; }
+
+        public Sphere(double radius)
+        {
+            Name = "Sphere";
+            Radius = radius;
+        }
+
+        public override double Volume()
+        {
+            return (4 * Math.PI * Math.Pow(Radius, 3)) / 3;
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"\nThe Sphere has a radius of {Radius}");
+        }
+    }
+}
