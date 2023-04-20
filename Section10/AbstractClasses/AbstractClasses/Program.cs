8d496c12790a5e21a294c6bd0c3b6d3e20aaeeec
Paul Stearns <celestialcoyote@gmail.com>
2023-04-20 16:28:17 -0700

    abstract and is keyword

--- Section10/AbstractClasses/AbstractClasses/Program.cs
+++ Section10/AbstractClasses/AbstractClasses/Program.cs
@@ -12,6 +12,23 @@ namespace AbstractClasses
             {
                 shape.GetInfo();
                 Console.WriteLine($"{shape.Name} has a volume of {shape.Volume()}.");
+
+                Cube iceCube = shape as Cube;
+
+                if (iceCube == null)
+                {
+                    Console.WriteLine("This shape is no cube.");
+                }
+
+                if (shape is Cube)
+                {
+                    Console.WriteLine("This shape is a cube.");
+                }
+
+                object cube1 = new Cube(7);
+                Cube cube2 = (Cube)cube1;
+
+                Console.WriteLine($"{cube2.Name} has a volume of {cube2.Volume()}.");
             }
 
 
