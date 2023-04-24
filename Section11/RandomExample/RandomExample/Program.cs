8044d9a9c5fda02aba9f1a01597f61388a4ff994
Paul Stearns <celestialcoyote@gmail.com>
2023-04-23 18:52:59 -0700

    random

---
+++ Section11/RandomExample/RandomExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RandomExample
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Random dice = new Random();
+
+            //int numEyes;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    numEyes = dice.Next(1, 7);
+            //    Console.WriteLine(numEyes);
+            //}
+
+
+            Random yesNoMaybe = new Random();
+
+            int answerNum = yesNoMaybe.Next(1, 4);
+
+            if (answerNum == 1)
+                Console.WriteLine("Yes");
+            else if (answerNum == 2)
+                Console.WriteLine("Maybe");
+            else
+                Console.WriteLine("No");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
