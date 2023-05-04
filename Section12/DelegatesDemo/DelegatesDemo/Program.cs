609d5bb0280ea3952420fe312889e159520cf02f
Paul Stearns <celestialcoyote@gmail.com>
2023-05-04 16:37:15 -0700

    delegates example

---
+++ Section12/DelegatesDemo/DelegatesDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace DelegatesDemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Anatoli" };
+
+            Console.WriteLine("----- before -----");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            names.RemoveAll(Filter);
+
+            Console.WriteLine("----- after -----");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        static bool Filter(string s)
+        {
+            return s.Contains("i");
+        }
+    }
+}
