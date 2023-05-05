de7c738a8775f332561b1a8ce6229584e0cf6984
Paul Stearns <celestialcoyote@gmail.com>
2023-05-04 17:28:36 -0700

    more delegates work

--- Section12/DelegatesDemo/DelegatesDemo/Program.cs
+++ Section12/DelegatesDemo/DelegatesDemo/Program.cs
@@ -6,28 +6,71 @@ namespace DelegatesDemo
 {
     class MainClass
     {
+        public delegate bool FilterDelegate(Person p);
+
         public static void Main(string[] args)
         {
-            List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Anatoli" };
+            //List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Anatoli" };
 
-            Console.WriteLine("----- before -----");
+            //Console.WriteLine("----- before -----");
 
-            foreach (string name in names)
-            {
-                Console.WriteLine(name);
-            }
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
 
-            names.RemoveAll(Filter);
+            //names.RemoveAll(Filter);
 
-            Console.WriteLine("----- after -----");
+            //Console.WriteLine("----- after -----");
 
-            foreach (string name in names)
-            {
-                Console.WriteLine(name);
-            }
+            //foreach (string name in names)
+            //{
+            //    Console.WriteLine(name);
+            //}
+
+            Person p1 = new Person() { Name = "Aiden", Age = 41 };
+            Person p2 = new Person() { Name = "Sif", Age = 69 };
+            Person p3 = new Person() { Name = "Walter", Age = 12 };
+            Person p4 = new Person() { Name = "Anatoli", Age = 25 };
+
+            List<Person> people = new List<Person>() { p1, p2, p3, p4 };
+
+            DisplayPeople("Kids", people, IsMinor);
+            DisplayPeople("Adults", people, IsAdult);
+            DisplayPeople("Seniors", people, IsSenior);
 
 
             Console.ReadKey();
+        }
+
+        static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        {
+            Console.WriteLine(title);
+
+            foreach (Person p in people)
+            {
+                if (filter(p))
+                {
+                    Console.WriteLine($"{p.Name}, {p.Age} years old.");
+            }
+            }
+            
+        }
+
+        //============= Filters ==============
+        static bool IsMinor(Person p)
+        {
+            return p.Age < 18;
+        }
+
+        static bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+
+        static bool IsSenior(Person p)
+        {
+            return p.Age >= 65;
         }
 
         static bool Filter(string s)
